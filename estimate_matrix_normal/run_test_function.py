6f943121e173d2841646c4ca682da1b7ac3b99a9
idanshabo <157521528+idanshabo@users.noreply.github.com>
2025-04-12 12:32:59 +0300

    Create run_test_function.py

---
+++ estimate_matrix_normal/run_test_function.py
@@ -0,0 +1,8 @@
+# Example usage
+if __name__ == "__main__":
+    test_matrix_normal_mle_functions_after_convergence(
+        convergence_scale=200,
+        proportions=[1/4, 1/3, 1/2, 1, 2, 3, 4],
+        sample_sizes_to_check=list(range(1, 6)),
+        num_runs=5,
+        save_folder="/content/drive/My Drive/Thesis/matrix normal estimation/"
