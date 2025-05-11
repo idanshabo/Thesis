ccecc6c1d2b0b572780e93436fc3d24933d6dcdb
idanshabo <157521528+idanshabo@users.noreply.github.com>
2025-05-11 22:15:11 +0300

    Update example_test_estimation_functions_all_plots.py

--- estimate_matrix_normal/test_estimation_functions/example_test_estimation_functions_all_plots.py
+++ estimate_matrix_normal/test_estimation_functions/example_test_estimation_functions_all_plots.py
@@ -3,9 +3,13 @@ from run_plot_functions import *
 
 # Example usage
 if __name__ == "__main__":
-    test_matrix_normal_mle_functions_after_convergence(
-        convergence_scale=200,
-        proportions=[1/4, 1/3, 1/2, 1, 2, 3, 4],
-        sample_sizes_to_check=list(range(1, 6)),
-        num_runs=5,
-        save_folder="/content/drive/My Drive/Thesis/matrix normal estimation/"
+    save_path = '/content/drive/My Drive/Thesis/matrix normal estimation/'
+    proportions = [1/4, 1/3, 1/2, 1]
+    scales = [2, 10, 50]
+    convergence_scale = 20
+
+    run_plot_convergence_by_matrix_size(save_path, proportions, scales)
+    run_error_metric_multi_scale(save_path, proportions, scales)
+    run_plot_error_by_proportion(save_path, proportions, convergence_scale)
+
+# There are more parameters adjustable, defined in the run_plot_functions file
