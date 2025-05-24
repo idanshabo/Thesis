7899f5e39ec54b44de13c2eec1ac66f46988429c
idanshabo <157521528+idanshabo@users.noreply.github.com>
2025-05-24 20:06:30 +0300

    Update convert_stockholm_to_fasta.py

--- convert_stockholm_to_fasta.py
+++ convert_stockholm_to_fasta.py
@@ -11,3 +11,4 @@ def convert_stockholm_to_fasta(stockholm_file_path, fasta_file_path=None):
     alignment = AlignIO.read(stockholm_file_path, "stockholm")
     AlignIO.write(alignment, fasta_file_path, "fasta")
     print(f"Converted file in stockholm format to fasta format in path {fasta_file_path}")
+    return(fasta_file_path)
