b5642076b014ed61508836c840e784073798a880
idanshabo <157521528+idanshabo@users.noreply.github.com>
2025-05-27 17:35:59 +0300

    Create calculate_covariance_matrix_pipeline.py

---
+++ calculate_covariance_matrix_pipeline.py
@@ -0,0 +1,12 @@
+from read_stockholm_file_and_print_content import read_stockholm_file_and_print_content
+from convert_stockholm_to_fasta import convert_stockholm_to_fasta
+from create_phylogenetic_tree.run_fasttree import run_fasttree
+
+
+def run_pipeline(MSA_file_path, print_file_content=False):
+    if print_file_content:
+        read_stockholm_file_and_print_content(MSA_file_path)
+    fasta_file = convert_stockholm_to_fasta(MSA_file_path)
+    phylogenetic_tree = run_fasttree(fasta_file)
+    
+    return phylogenetic_tree
