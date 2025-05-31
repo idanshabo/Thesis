d1eb8c317432513da93e86a4cfc401309a72e4c7
idanshabo <157521528+idanshabo@users.noreply.github.com>
2025-05-31 22:30:28 +0300

    Update create_esm_embeddings_pipeline.py

--- create_esm_embeddings_pipeline.py
+++ create_esm_embeddings_pipeline.py
@@ -1,10 +1,7 @@
 from read_stockholm_file_and_print_content import read_stockholm_file_and_print_content
 from convert_stockholm_to_fasta import convert_stockholm_to_fasta
 from create_phylogenetic_tree.run_fasttree import run_fasttree
-
-    fasta_file = '/content/drive/MyDrive/protein_data/PF03618.fasta'
-    save_dir = "embeddings_output"  
-    model = ESMC.from_pretrained("esmc_300m")
+from create_esm_embeddings import create_esm_embeddings_from_fasta
 
 
 def run_pipeline(MSA_file_path, model=ESMC.from_pretrained("esmc_300m"), seve_dir=None):
