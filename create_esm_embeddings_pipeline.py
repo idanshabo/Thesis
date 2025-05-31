52b92a1891bcae8bdd66fa6f989db8dc8c672669
idanshabo <157521528+idanshabo@users.noreply.github.com>
2025-05-31 22:30:38 +0300

    Update create_esm_embeddings_pipeline.py

--- create_esm_embeddings_pipeline.py
+++ create_esm_embeddings_pipeline.py
@@ -1,6 +1,3 @@
-from read_stockholm_file_and_print_content import read_stockholm_file_and_print_content
-from convert_stockholm_to_fasta import convert_stockholm_to_fasta
-from create_phylogenetic_tree.run_fasttree import run_fasttree
 from create_esm_embeddings import create_esm_embeddings_from_fasta
 
 
