c0109e3ec51c4a4e532d5c2f36ec3641913c6837
idanshabo <157521528+idanshabo@users.noreply.github.com>
2025-05-31 22:45:07 +0300

    Update create_esm_embeddings_pipeline.py

--- create_esm_embeddings_pipeline.py
+++ create_esm_embeddings_pipeline.py
@@ -1,6 +1,6 @@
 from create_esm_embeddings import create_esm_embeddings_from_fasta
 
 
-def run_pipeline(MSA_file_path, model=ESMC.from_pretrained("esmc_300m"), seve_dir=None):
+def run_pipeline(MSA_file_path, seve_dir=None):
     create_esm_embeddings_from_fasta(MSA_file_path, save_dir)
     return save_dir
