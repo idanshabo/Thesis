23a3a7d9d41b97a686e840ead8aad2536362eb40
idanshabo <157521528+idanshabo@users.noreply.github.com>
2025-06-03 06:21:57 +0300

    Update create_esm_embeddings_pipeline.py

--- create_esm_embeddings_pipeline.py
+++ create_esm_embeddings_pipeline.py
@@ -7,4 +7,4 @@ def run_pipeline(MSA_file_path, output_path=None):
         base_path = os.path.splitext(MSA_file_path)[0].replace('.fasta', '')
         output_path = base_path + '/embeddings_output'
     create_esm_embeddings_from_fasta(MSA_file_path, output_path)
-    return save_dir
+    return output_path
