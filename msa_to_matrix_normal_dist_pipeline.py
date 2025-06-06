b0a442b4dd64b588bad45928926a378270613061
idanshabo <157521528+idanshabo@users.noreply.github.com>
2025-06-06 11:59:07 +0300

    Update msa_to_matrix_normal_dist_pipeline.py

--- msa_to_matrix_normal_dist_pipeline.py
+++ msa_to_matrix_normal_dist_pipeline.py
@@ -2,11 +2,22 @@ from read_stockholm_file_and_print_content import read_stockholm_file_and_print_
 from convert_stockholm_to_fasta import convert_stockholm_to_fasta
 from create_phylogenetic_tree.run_fasttree import run_fasttree
 from phylogenetic_tree_to_covariance_matrix import tree_to_covariance_matrix
+from handling_esm_embeddings.create_esm_embeddings import create_esm_embeddings_from_fasta
+from handling_esm_embeddings.convert_embeddings_to_one_mean_embedding import convert_embeddings_to_one_mean_embedding
+import os
 
-def run_pipeline(MSA_file_path, print_file_content=False):
+
+def run_pipeline(MSA_file_path, print_file_content=False, output_path=None):
     if print_file_content:
         read_stockholm_file_and_print_content(MSA_file_path)
     fasta_file_path = convert_stockholm_to_fasta(MSA_file_path)
     phylogenetic_tree_path = run_fasttree(fasta_file_path)
     cov_mat = tree_to_covariance_matrix(phylogenetic_tree_path)
+    if not output_path:
+        base_path = os.path.splitext(MSA_file_path)[0].replace('.fasta', '')
+        output_path = base_path + '/embeddings_output'
+    create_esm_embeddings_from_fasta(MSA_file_path, output_path)
+    mean_embeddings_output_path = convert_embeddings_to_one_mean_embedding(output_path)
+    
+    return mean_embeddings_output_path
     return cov_mat
