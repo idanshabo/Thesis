13b3d68cb178a18d8719b26c1a3b96939d24713c
idanshabo <157521528+idanshabo@users.noreply.github.com>
2025-06-06 17:53:49 +0300

    Update msa_to_matrix_normal_dist_pipeline.py

--- pipeline_files/msa_to_matrix_normal_dist_pipeline.py
+++ pipeline_files/msa_to_matrix_normal_dist_pipeline.py
@@ -6,6 +6,7 @@ from handling_esm_embeddings.create_esm_embeddings import create_esm_embeddings_
 from handling_esm_embeddings.convert_embeddings_to_one_mean_embedding import convert_embeddings_to_one_mean_embedding
 from estimate_matrix_normal.estimate_matrix_normal import matrix_normal_mle_fixed_u, matrix_normal_mle
 from align_embeddings_with_covariance import align_embeddings_with_covariance
+from check_matching_names import check_matching_names
 import os
 
 
@@ -20,6 +21,9 @@ def run_pipeline(MSA_file_path, print_file_content=False, output_path=None):
         output_path = base_path + '/embeddings_output'
     create_esm_embeddings_from_fasta(MSA_file_path, output_path)
     mean_embeddings_dict_path = convert_embeddings_to_one_mean_embedding(output_path)
+    matching_names = check_matching_names(cov_mat, mean_embeddings_dict_path)
+    if not matching_names:
+        return(False)
     embeddings_matrix, protein_list = align_embeddings_with_covariance(cov_mat, mean_embeddings_dict_path)
     Mean_mat, V_mat = matrix_normal_mle_fixed_u(X=[embeddings_matrix], U=cov_mat)
     return Mean_mat, V_mat, cov_mat
