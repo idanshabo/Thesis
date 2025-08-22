e926b12f2d6adf8e778d5ee962964a104229e8eb
idanshabo <157521528+idanshabo@users.noreply.github.com>
2025-08-22 09:24:39 +0300

    Update create_esm_embeddings_pipeline.py

--- pipeline_files/create_esm_embeddings_pipeline.py
+++ pipeline_files/create_esm_embeddings_pipeline.py
@@ -6,12 +6,12 @@ import os
 
 
 def run_pipeline(MSA_file_path, output_path=None, print_file_content=False):
-    if not output_path:
-        base_path = os.path.splitext(MSA_file_path)[0].replace('.fasta', '')
-        output_path = base_path + '/embeddings_output'
     if print_file_content:
         read_stockholm_file_and_print_content(MSA_file_path)
     fasta_file_path = convert_stockholm_to_fasta(MSA_file_path)
+    if not output_path:
+        base_path = os.path.splitext(fasta_file_path)[0].replace('.fasta', '')
+        output_path = base_path + '/embeddings_output'
     create_esm_embeddings_from_fasta(fasta_file_path, output_path)
     mean_embeddings_output_path = convert_embeddings_to_one_mean_embedding(output_path)
     return mean_embeddings_output_path
