80808e3ed9d6cf7ffc75ff1653285a82604bf6f3
idanshabo <157521528+idanshabo@users.noreply.github.com>
2025-08-22 17:30:39 +0300

    Update assure_cov_mat_positive_definite.py

--- assure_cov_mat_positive_definite.py
+++ assure_cov_mat_positive_definite.py
@@ -6,7 +6,7 @@ def assure_cov_mat_positive_definite(path_to_matrix, alpha=1e-6):
     Adds a small value (alpha) to the diagonal of the matrix to make it positive definite.
     """
     matrix = pd.read_csv(path_to_matrix, index_col=0)  # Assuming the first column is the index (if any)
-    matrix = matrix.values  # Convert to a numpy array for numerical operations
+    matrix = matrix.to_numpy()  # Convert to a numpy array for numerical operations
     
     eigenvalues = np.linalg.eigvals(matrix)
     smallest_eigenvalue = np.min(np.real(eigenvalues)) 
