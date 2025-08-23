507d4abf1d37b970c0b00b59047de9c885937740
idanshabo <157521528+idanshabo@users.noreply.github.com>
2025-08-23 13:17:35 +0300

    Update estimate_matrix_normal.py

--- estimate_matrix_normal/estimate_matrix_normal.py
+++ estimate_matrix_normal/estimate_matrix_normal.py
@@ -269,5 +269,9 @@ def matrix_normal_mle_fixed_u(X: List[np.ndarray],
         if step >= max_iter:
             print(f"Warning: Maximum iterations ({max_iter}) reached without convergence")
             break
-
-    return M, V_plus, U
+    output_dir = os.path.dirname(U_path)
+    mean_output_path = output_dir + 'Mean'
+    embeddings_cov_output_path = output_dir + 'embeddings_cov_mat'                         
+    M.to_csv(mean_output_path)
+    V_plus.to_csv(embeddings_cov_output_path)     
+    return mean_output_path, embeddings_cov_output_path, U_path
