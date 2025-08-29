6e9f673dc5b6f56161ab7a8bbddba1fefce2ec2b
idanshabo <157521528+idanshabo@users.noreply.github.com>
2025-08-29 15:50:13 +0300

    Update create_normalized_mean_embeddings_matrix.py

--- handling_esm_embeddings/create_normalized_mean_embeddings_matrix.py
+++ handling_esm_embeddings/create_normalized_mean_embeddings_matrix.py
@@ -12,12 +12,19 @@ def normalize_matrix(matrix):
     return normalized_matrix
 
 
+
+    
+    cov_matrix = pd.read_csv(cov_matrix_path, index_col=0)
+    cov_names = list(cov_matrix.index)
+    file_names = data_dict['file_names']
+    embeddings = data_dict['embeddings']
+    
 def create_normalized_mean_embeddings_matrix(fasta_file_path, output_path=None):
     if not output_path:
         output_path = os.path.join(os.path.dirname(fasta_file_path), 'embeddings_output')
     create_esm_embeddings_from_fasta(fasta_file_path, output_path)
     mean_embeddings_output_path = convert_embeddings_to_one_mean_embedding(output_path)
-
+    data_dict = torch.load(data_dict_path, map_location='cpu')
     
     normalized_mean_embeddings = normalize_matrix()
     
