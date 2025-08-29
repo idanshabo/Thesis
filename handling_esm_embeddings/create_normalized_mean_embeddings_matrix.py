0a9576e7e9b696f080aabe23e8a40b2982aa7f9f
idanshabo <157521528+idanshabo@users.noreply.github.com>
2025-08-29 15:46:22 +0300

    Update create_normalized_mean_embeddings_matrix.py

--- handling_esm_embeddings/create_normalized_mean_embeddings_matrix.py
+++ handling_esm_embeddings/create_normalized_mean_embeddings_matrix.py
@@ -1,4 +1,7 @@
-from handling_esm_embeddings import ,
+from handling_esm_embeddings.create_esm_embeddings import create_esm_embeddings_from_fasta
+from handling_esm_embeddings.convert_embeddings_to_one_mean_embedding import convert_embeddings_to_one_mean_embedding
+import os
+
 
 def normalize_matrix(matrix):
     mean = np.mean(matrix)
@@ -9,8 +12,13 @@ def normalize_matrix(matrix):
     return normalized_matrix
 
 
-def create_normalized_mean_embeddings_matrix(fasta_file, output_path):
-    dsfsdf
+def create_normalized_mean_embeddings_matrix(fasta_file_path, output_path=None):
+    if not output_path:
+        output_path = os.path.join(os.path.dirname(fasta_file_path), 'embeddings_output')
+    create_esm_embeddings_from_fasta(fasta_file_path, output_path)
+    mean_embeddings_output_path = convert_embeddings_to_one_mean_embedding(output_path)
+
+    
     normalized_mean_embeddings = normalize_matrix()
     
     return 
