79c59d11282f78ff727d92857814b1ef9195da77
idanshabo <157521528+idanshabo@users.noreply.github.com>
2025-11-28 11:30:41 +0200

    Update msa_to_split_full_pipeline.py

--- pipeline_files/msa_to_split_full_pipeline.py
+++ pipeline_files/msa_to_split_full_pipeline.py
@@ -3,12 +3,9 @@ from convert_stockholm_to_fasta import convert_stockholm_to_fasta
 from create_phylogenetic_tree.run_fasttree import run_fasttree
 from phylogenetic_tree_to_covariance_matrix import tree_to_covariance_matrix
 from handling_esm_embeddings.create_normalized_mean_embeddings_matrix import create_normalized_mean_embeddings_matrix
-from estimate_matrix_normal.estimate_matrix_normal import matrix_normal_mle_fixed_u, matrix_normal_mle
-from align_embeddings_with_covariance import align_embeddings_with_covariance
 from check_matching_names import check_matching_names
 import os
 import torch
-
 from evaluate_split_options import evaluate_top_splits
 
 
@@ -20,7 +17,15 @@ def run_pipeline(MSA_file_path, print_file_content=False, output_path=None):
     cov_mat_path = tree_to_covariance_matrix(phylogenetic_tree_path)
     if not output_path:
         output_path = os.path.join(os.path.dirname(fasta_file_path), 'embeddings_output')
-
+    
     normalized_mean_embeddings_path = create_normalized_mean_embeddings_matrix(fasta_file_path, output_path)
-    evaluate_top_splits(phylogenetic_tree_path, cov_mat_path, normalized_mean_embeddings_path, output_path=os.path.dirname(fasta_file_path), k=5, target_pca_variance=0.99, standardize=True)
-    return Mean_mat_path, V_mat_path, cov_mat_path
+    significant_splits_output_path = os.path.join(os.path.dirname(fasta_file_path), 'significant_splits')
+    results = evaluate_top_splits(phylogenetic_tree_path, cov_mat_path, normalized_mean_embeddings_path, output_path=significant_splits_output_path, k=5, target_pca_variance=0.99, standardize=True)
+    
+    files = os.listdir(significant_splits_output_path)
+    if files:
+        for file_name in files:
+            file_path = os.path.join(significant_splits_output_path, file_name)
+            print("Processing:", file_path)
+            # your code here
+    return results
