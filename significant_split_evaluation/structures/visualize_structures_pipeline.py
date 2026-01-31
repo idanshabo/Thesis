559ef5e12cb8d5ea1395d792197fd07f83859375
idanshabo <157521528+idanshabo@users.noreply.github.com>
2026-01-31 19:35:16 +0200

    Update visualize_structures_pipeline.py

--- significant_split_evaluation/structures/visualize_structures_pipeline.py
+++ significant_split_evaluation/structures/visualize_structures_pipeline.py
@@ -4,7 +4,7 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
-import logomaker  # <--- NEW DEPENDENCY
+import logomaker
 from Bio import SeqIO
 
 # Import the local modules
