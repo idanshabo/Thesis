447e66bf0b0b6bdd33edf3a5984d1bd550298254
idanshabo <157521528+idanshabo@users.noreply.github.com>
2026-03-02 16:15:40 +0200

    Update create_normalized_mean_embeddings_matrix.py

--- handling_esm_embeddings/create_normalized_mean_embeddings_matrix.py
+++ handling_esm_embeddings/create_normalized_mean_embeddings_matrix.py
@@ -1,4 +1,4 @@
-from handling_esm_embeddings.create_esm_embeddings import create_esm_embeddings_from_fasta
+from handling_esm_embeddings.create_esm_embeddings import generate_embeddings
 from handling_esm_embeddings.convert_embeddings_to_one_mean_embedding import convert_embeddings_to_one_mean_embedding
 import os
 import numpy as np
@@ -17,7 +17,7 @@ def create_normalized_mean_embeddings_matrix(fasta_file_path, embedding_mode, ou
         output_path = os.path.join(os.path.dirname(fasta_file_path), 'embeddings_output')
     normalized_mean_embeddings_output_path = output_path + '/mean_embeddings_output/normalized_mean_protein_embeddings.pt'
     
-    create_esm_embeddings_from_fasta(fasta_file_path, output_path, embedding_mode)
+    generate_embeddings(fasta_file_path, output_path, embedding_mode)
     mean_embeddings_path = convert_embeddings_to_one_mean_embedding(output_path)
     mean_embeddings_tensor = torch.load(mean_embeddings_path, map_location='cpu')
     file_names = mean_embeddings_tensor['file_names']
